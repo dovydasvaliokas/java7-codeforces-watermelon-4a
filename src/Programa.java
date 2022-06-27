3b1833aa37d7c2f746188808b7fe7ab82593b504
dovydasvaliokas <74957396+dovydasvaliokas@users.noreply.github.com>
2022-06-27 12:49:14 +0300

    Sukurta funkcija, kuri konvertuoja boolean i string (yes no)

--- src/Programa.java
+++ src/Programa.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Programa {
     public static void main(String[] args) {
+        Scanner skaitytuvas = new Scanner(System.in);
+        int w = skaitytuvas.nextInt();          // arbūzo svoris
+        System.out.println("w = " + w);
 
     }
 
 
+    /**
+     * Funkcija patikrina ar skaičius yra lyginis
+     * @param x tikrinamas skaičius
+     * @return grąžina true, jei lyginis; grąžina false, jei nelyginis.
+     */
     public boolean arSkaiciusLyginis(int x) {
         return x % 2 == 0;
+    }
+
+
+    /**
+     * Funkcija konvertuoja boolean reikšmę į paprastas angliškas žodines reikšmes (yes ir no)
+     * @param b boolean reikšmė
+     * @return Yes jeigu true, No jeigu false
+     */
+    public String konvertuojaYesIrNo(boolean b) {
+        if (b) {
+            return "Yes";
+        }
+        else {
+            return "No";
+        }
     }
 }
